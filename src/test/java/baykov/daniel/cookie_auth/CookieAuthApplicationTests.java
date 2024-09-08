d5d905167d00ef0119fb4f188bc7c1f7f1b412c1
Daniel Baykov <danibaikov@gmail.com>
2024-09-08 17:32:21 +0300

    Initial commit

---
+++ src/test/java/baykov/daniel/cookie_auth/CookieAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package baykov.daniel.cookie_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookieAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
