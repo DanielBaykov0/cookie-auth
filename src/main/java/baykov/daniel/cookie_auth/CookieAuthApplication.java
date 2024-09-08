73624be81867b95e13be1a9875b8234b98a76468
Daniel Baykov <danibaikov@gmail.com>
2024-09-08 17:35:30 +0300

    main class

---
+++ src/main/java/baykov/daniel/cookie_auth/CookieAuthApplication.java
@@ -0,0 +1,12 @@
+package baykov.daniel.cookie_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookieAuthApplication.class, args);
+	}
+}
